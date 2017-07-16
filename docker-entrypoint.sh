a35751c2c941210dae153d55486bbb5eeb8329a4
Richard Carls <richard.j.carls@gmail.com>
2017-07-16 00:41:24 +0000

    Fix "server" usage of entrypoint script
    - now forces exec of hugo for easier usage
    - HUGO_BASE_URL is now respected
    - checks for existing user before creating (for derived images)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 set -e
 
-# Ignore "--user -u" and non-hugo commands
-if [ "$(id -u)" = '0' ] && [ "$1" = 'hugo' ]; then
+# Always call hugo
+if [ "$1" != 'hugo' ]; then
+    set -- hugo "$@"
+fi
+
+# Ignore "--user -u" option
+if [ "$(id -u)" = '0' ]; then
+
+    # Set "--bind" for server
+    if [ "$2" = 'server' ]; then
+        # Optionally append "--baseURL" and disable "--appendPort"
+        if [ -n "${HUGO_BASE_URL}" ]; then set -- "$@" --baseURL "${HUGO_BASE_URL}" --appendPort=false; fi
+
+        set -- "$@" --bind "0.0.0.0"
+    fi
+
     # Step down to user
     USER_MOUNT_UID="$(stat -c '%u' /hugo/site)"
     if [ "${USER_MOUNT_UID}" != '0' ]; then
-        addgroup -g "${USER_MOUNT_UID}" hugo
-        adduser -u "${USER_MOUNT_UID}" -D -G hugo hugo
+        if [ -z "$(getent passwd hugo)" ]; then
+            addgroup -g "${USER_MOUNT_UID}" hugo
+            adduser -u "${USER_MOUNT_UID}" -D -G hugo hugo
+        fi
 
-        exec su-exec hugo "$@" --baseURL "${HUGO_BASE_URL}" --appendPort=false --bind "0.0.0.0"
+        set -- su-exec hugo "$@"
     fi
-
-    exec "$@" --baseURL "${HUGO_BASE_URL}" --appendPort=false --bind "0.0.0.0"
 fi
+
+echo "Executing CMD \"$@\" ..."
 
 exec "$@"
